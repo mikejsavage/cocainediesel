42b1a13c19b2f70dcfb5b262662ee722019fea6e
ObaniGemini <robin@pi-et-ro.net>
2019-01-27 17:30:11 +0100

    Fix it with CG_CenterPrint

--- source/cgame/cg_view.cpp
+++ source/cgame/cg_view.cpp
@@ -111,8 +111,7 @@ static void CG_AddLocalSounds( void ) {
 				if( 1 + remainingSeconds < 4 ) {
 					struct sfx_s *sound = trap_S_RegisterSound( va( S_ANNOUNCER_COUNTDOWN_COUNT_1_to_3_SET_1_to_2, 1 + remainingSeconds, 1 ) );
 					CG_AddAnnouncerEvent( sound, false );
-					CG_ClearAwards();
-					CG_AddAward( va("%i", remainingSeconds + 1) );
+					CG_CenterPrint( va("%i", remainingSeconds + 1) );
 				}
 
 				lastSecond = remainingSeconds;
@@ -143,8 +142,6 @@ static void CG_FlashGameWindow( void ) {
 		switch( newState ) {
 			case MATCH_STATE_COUNTDOWN:
 			case MATCH_STATE_PLAYTIME:
-				CG_ClearAwards();
-				break;
 			case MATCH_STATE_POSTMATCH:
 				flash = true;
 				break;

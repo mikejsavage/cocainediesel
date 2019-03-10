f2bf2ce90cfd4853dd190713b83e1ba8cafc9cec
Michael Savage <mikejsavage@gmail.com>
2019-03-10 20:58:53 +0200

    Tabs

--- source/qcommon/types.h
+++ source/qcommon/types.h
@@ -73,11 +73,11 @@ struct Mat2 {
 	Vec2 col0, col1;
 
 	Mat2() { }
-        constexpr Mat2( Vec2 c0, Vec2 c1 ) : col0( c0 ), col1( c1 ) { }
-        constexpr Mat2( float e00, float e01, float e10, float e11 ) : col0( e00, e10 ), col1( e01, e11 ) { }
+	constexpr Mat2( Vec2 c0, Vec2 c1 ) : col0( c0 ), col1( c1 ) { }
+	constexpr Mat2( float e00, float e01, float e10, float e11 ) : col0( e00, e10 ), col1( e01, e11 ) { }
 
-        Vec2 row0() const { return Vec2( col0.x, col1.x ); }
-        Vec2 row1() const { return Vec2( col0.y, col1.y ); }
+	Vec2 row0() const { return Vec2( col0.x, col1.x ); }
+	Vec2 row1() const { return Vec2( col0.y, col1.y ); }
 
 	static constexpr Mat2 I() { return Mat2( 1, 0, 0, 1 ); }
 };
@@ -86,16 +86,16 @@ struct Mat3 {
 	Vec3 col0, col1, col2;
 
 	Mat3() { }
-        constexpr Mat3( Vec3 c0, Vec3 c1, Vec3 c2 ) : col0( c0 ), col1( c1 ), col2( c2 ) { }
+	constexpr Mat3( Vec3 c0, Vec3 c1, Vec3 c2 ) : col0( c0 ), col1( c1 ), col2( c2 ) { }
 	constexpr Mat3(
 		float e00, float e01, float e02,
 		float e10, float e11, float e12,
 		float e20, float e21, float e22
 	) : col0( e00, e10, e20 ), col1( e01, e11, e21 ), col2( e02, e12, e22 ) { }
 
-        Vec3 row0() const { return Vec3( col0.x, col1.x, col2.x ); }
-        Vec3 row1() const { return Vec3( col0.y, col1.y, col2.y ); }
-        Vec3 row2() const { return Vec3( col0.z, col1.z, col2.z ); }
+	Vec3 row0() const { return Vec3( col0.x, col1.x, col2.x ); }
+	Vec3 row1() const { return Vec3( col0.y, col1.y, col2.y ); }
+	Vec3 row2() const { return Vec3( col0.z, col1.z, col2.z ); }
 
 	static constexpr Mat3 I() {
 		return Mat3(
@@ -110,18 +110,18 @@ struct alignas( 16 ) Mat4 {
 	Vec4 col0, col1, col2, col3;
 
 	Mat4() { }
-        constexpr Mat4( Vec4 c0, Vec4 c1, Vec4 c2, Vec4 c3 ) : col0( c0 ), col1( c1 ), col2( c2 ), col3( c3 ) { }
+	constexpr Mat4( Vec4 c0, Vec4 c1, Vec4 c2, Vec4 c3 ) : col0( c0 ), col1( c1 ), col2( c2 ), col3( c3 ) { }
 	constexpr Mat4(
-                float e00, float e01, float e02, float e03,
-                float e10, float e11, float e12, float e13,
-                float e20, float e21, float e22, float e23,
-                float e30, float e31, float e32, float e33
-        ) : col0( e00, e10, e20, e30 ), col1( e01, e11, e21, e31 ), col2( e02, e12, e22, e32 ), col3( e03, e13, e23, e33 ) { }
+		float e00, float e01, float e02, float e03,
+		float e10, float e11, float e12, float e13,
+		float e20, float e21, float e22, float e23,
+		float e30, float e31, float e32, float e33
+	) : col0( e00, e10, e20, e30 ), col1( e01, e11, e21, e31 ), col2( e02, e12, e22, e32 ), col3( e03, e13, e23, e33 ) { }
 
-        Vec4 row0() const { return Vec4( col0.x, col1.x, col2.x, col3.x ); }
-        Vec4 row1() const { return Vec4( col0.y, col1.y, col2.y, col3.y ); }
-        Vec4 row2() const { return Vec4( col0.z, col1.z, col2.z, col3.z ); }
-        Vec4 row3() const { return Vec4( col0.w, col1.w, col2.w, col3.w ); }
+	Vec4 row0() const { return Vec4( col0.x, col1.x, col2.x, col3.x ); }
+	Vec4 row1() const { return Vec4( col0.y, col1.y, col2.y, col3.y ); }
+	Vec4 row2() const { return Vec4( col0.z, col1.z, col2.z, col3.z ); }
+	Vec4 row3() const { return Vec4( col0.w, col1.w, col2.w, col3.w ); }
 
 	static constexpr Mat4 I() {
 		return Mat4(

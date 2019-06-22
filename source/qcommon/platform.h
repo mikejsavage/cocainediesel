2ac4571dabc4976a82b72be436d9e7d9260f8cb2
Michael Savage <mikejsavage@gmail.com>
2019-06-22 17:46:07 +0300

    Add platform detection header

---
+++ source/qcommon/platform.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#if defined( _WIN32 )
+#  define PLATFORM_WINDOWS 1
+#elif defined( __linux__ )
+#  define PLATFORM_LINUX 1
+#else
+#  error new platform
+#endif
+
+#if defined( _MSC_VER )
+#  define COMPILER_MSVC 1
+#elif defined( __clang__ )
+#  define COMPILER_CLANG 1
+#  define COMPILER_GCCORCLANG 1
+#elif defined( __GNUC__ )
+#  define COMPILER_GCC 1
+#  define COMPILER_GCCORCLANG 1
+#else
+#  error new compiler
+#endif

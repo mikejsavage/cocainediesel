4fa73ab4bf9d1836b0173707a94b162e9419d314
Michael Savage <mikejsavage@gmail.com>
2019-09-07 01:47:09 +0300

    Compile fix

--- source/win32/win_fs.cpp
+++ source/win32/win_fs.cpp
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "qcommon/qcommon.h"
+#include "qcommon/string.h"
 #include "qcommon/fs.h"
 
 #include "qcommon/sys_fs.h"

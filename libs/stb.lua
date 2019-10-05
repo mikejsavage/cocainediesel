6119ac8bdd4c70fa93a98e51f53a3e64153e809f
Michael Savage <mikejsavage@gmail.com>
2019-10-05 03:04:23 +0300

    stb_vorbis warnings

--- libs/stb.lua
+++ libs/stb.lua
@@ -6,4 +6,4 @@ lib( "stb_image_write", { "libs/stb/stb_image_write.cpp" } )
 
 lib( "stb_vorbis", { "libs/stb/stb_vorbis.cpp" } )
 msvc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "/O2 /wd4244 /wd4245 /wd4456 /wd4457 /wd4701" )
-gcc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "-O2 -Wno-unused-value" )
+gcc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "-O2 -Wno-unused-value -Wno-maybe-uninitialized" )

4870ad46e7d229d635e06e27b42540e384e528b2
Michael Savage <mikejsavage@gmail.com>
2019-10-06 03:13:20 +0300

    Add missing shader

---
+++ base/glsl/decal.glsl
@@ -0,0 +1,33 @@
+#include "include/common.glsl"
+#include "include/uniforms.glsl"
+#include "include/fog.glsl"
+
+uniform sampler2D u_DepthTexture;
+
+#if VERTEX_SHADER
+
+in vec4 a_Position;
+
+void main() {
+	gl_Position = a_Position;
+}
+
+#else
+
+out vec4 f_Albedo;
+
+vec3 WorldPosition() {
+	vec2 uv = gl_FragCoord.xy / u_ViewportSize;
+	float depth = qf_texture( u_DepthTexture, uv ).r;
+
+	vec4 clip = vec4( vec3( uv, depth ) * 2.0 - 1.0, 1.0 );
+	vec4 world = u_InverseV * u_InverseP * clip;
+	return world.xyz / world.w;
+}
+
+void main() {
+	vec3 world = WorldPosition();
+	f_Albedo = vec4( mod( world * 0.001, 1.0 ), 1.0 );
+}
+
+#endif

b0998d18ce6a07dfe3b78266beabf16c60316536
Michael Savage <mikejsavage@gmail.com>
2019-12-24 17:17:10 +0200

    Build fix

--- source/qcommon/version.h
+++ source/qcommon/version.h
@@ -1,4 +1,5 @@
+#include "qcommon/types.h"
 #include "qcommon/hash.h"
 #include "gitversion.h"
 
-constexpr int APP_PROTOCOL_VERSION = int( Hash32_CT( APP_VERSION, sizeof( APP_VERSION ) ) % INT_MAX );
+constexpr int APP_PROTOCOL_VERSION = int( Hash32_CT( APP_VERSION, sizeof( APP_VERSION ) ) % S32_MAX );

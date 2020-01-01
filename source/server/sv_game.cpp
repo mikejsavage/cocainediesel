70fd70111fca4803ff8f8750a5f455ba61a726ca
Michael Savage <mikejsavage@gmail.com>
2020-01-01 15:55:33 +0200

    sv_defaultmap carfentanil

--- source/server/sv_game.cpp
+++ source/server/sv_game.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "server.h"
+#include "server/server.h"
 #include "qcommon/version.h"
 
 game_export_t *ge;
--- source/server/sv_main.cpp
+++ source/server/sv_main.cpp
@@ -605,7 +605,7 @@ void SV_Init( void ) {
 
 	sv_iplimit = Cvar_Get( "sv_iplimit", "3", CVAR_ARCHIVE );
 
-	sv_defaultmap =         Cvar_Get( "sv_defaultmap", "dust", CVAR_ARCHIVE );
+	sv_defaultmap =         Cvar_Get( "sv_defaultmap", "carfentanil", CVAR_ARCHIVE );
 	sv_reconnectlimit =     Cvar_Get( "sv_reconnectlimit", "3", CVAR_ARCHIVE );
 	sv_maxclients =         Cvar_Get( "sv_maxclients", "16", CVAR_ARCHIVE | CVAR_SERVERINFO | CVAR_LATCH );
 

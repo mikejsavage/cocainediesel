07ed3cd984f355ec7474f56549e02a8d27b2f4fb
Michael Savage <mikejsavage@gmail.com>
2020-02-08 23:49:18 +0200

    Nuke SDL

--- libs/imgui.lua
+++ libs/imgui.lua
@@ -2,5 +2,4 @@ lib( "imgui", { "libs/imgui/*.cpp" } )
 
 obj_cxxflags( "libs/imgui/imgui_freetype.cpp", "-I libs/freetype" )
 obj_cxxflags( "libs/imgui/imgui_impl_opengl3.cpp", "-DIMGUI_IMPL_OPENGL_LOADER_CUSTOM=\\\"glad/glad.h\\\"" )
-obj_cxxflags( "libs/imgui/imgui_impl_sdl.cpp", "-I libs/sdl" )
 obj_cxxflags( "libs/imgui/imgui_impl_glfw.cpp", "-I libs/glfw3 -DGLFW_INCLUDE_NONE" )

99fcb7361c264d481381ce3a8b10307863c08124
Michael Savage <mikejsavage@gmail.com>
2020-02-09 00:54:00 +0200

    Check if we need to run vcvarsall

--- vs2019.bat
+++ vs2019.bat
@@ -1,3 +1,3 @@
-call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_amd64
+if not defined INCLUDE call "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_amd64
 ggbuild\lua.exe make.lua > build.ninja
 ggbuild\ninja.exe

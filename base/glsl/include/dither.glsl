0ccafc382e1b10fbc991cf00a2c4219cf2582d83
Michael Savage <mikejsavage@gmail.com>
2020-04-24 10:47:29 +0300

    Not like this

--- base/glsl/include/dither.glsl
+++ base/glsl/include/dither.glsl
@@ -8,7 +8,7 @@ layout( std140 ) uniform u_BlueNoiseTextureParams {
 
 vec3 Dither() {
 	vec3 noise = texture( u_BlueNoiseTexture, gl_FragCoord.xy / u_BlueNoiseTextureSize ).xxx;
-	return ( noise - vec3( 0.5 ) ) / 16.0;
+	return ( noise - vec3( 0.5 ) ) / 256.0;
 }
 
 #endif

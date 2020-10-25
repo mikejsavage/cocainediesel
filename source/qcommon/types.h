79f31920201b0856d1c7c8ada5a50d99737ff95a
Michael Savage <mikejsavage@gmail.com>
2020-10-25 13:56:08 +0200

    Move some allocator stuff out of types.h

--- source/qcommon/types.h
+++ source/qcommon/types.h
@@ -45,7 +45,7 @@ typedef uint64_t u64;
 #define U64 UINT64_C
 
 /*
- * allocators
+ * Allocator
  */
 
 struct Allocator {
@@ -60,53 +60,7 @@ struct Allocator {
 	char * operator()( const char * fmt, const Rest & ... rest );
 };
 
-struct ArenaAllocator;
-struct TempAllocator final : public Allocator {
-	TempAllocator() = default;
-	TempAllocator( const TempAllocator & other );
-	~TempAllocator();
-
-	void operator=( const TempAllocator & ) = delete;
-
-	void * try_allocate( size_t size, size_t alignment, const char * func, const char * file, int line );
-	void * try_reallocate( void * ptr, size_t current_size, size_t new_size, size_t alignment, const char * func, const char * file, int line );
-	void deallocate( void * ptr, const char * func, const char * file, int line );
-
-private:
-	ArenaAllocator * arena;
-	u8 * old_cursor;
-
-	friend struct ArenaAllocator;
-};
-
-struct ArenaAllocator final : public Allocator {
-	ArenaAllocator() = default;
-	ArenaAllocator( void * mem, size_t size );
-
-	void * try_allocate( size_t size, size_t alignment, const char * func, const char * file, int line );
-	void * try_reallocate( void * ptr, size_t current_size, size_t new_size, size_t alignment, const char * func, const char * file, int line );
-	void deallocate( void * ptr, const char * func, const char * file, int line );
-
-	TempAllocator temp();
-
-	void clear();
-	void * get_memory();
-
-	float max_utilisation() const;
-
-private:
-	u8 * memory;
-	u8 * top;
-	u8 * cursor;
-	u8 * cursor_max;
-
-	u32 num_temp_allocators;
-
-	void * try_temp_allocate( size_t size, size_t alignment, const char * func, const char * file, int line );
-	void * try_temp_reallocate( void * ptr, size_t current_size, size_t new_size, size_t alignment, const char * func, const char * file, int line );
-
-	friend struct TempAllocator;
-};
+struct TempAllocator;
 
 #if COMPILER_MSVC
 #define __PRETTY_FUNCTION__ __FUNCSIG__

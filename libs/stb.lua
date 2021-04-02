4e5f0ddb9640bca7a35688dc76c9cf7f93e3ec8f
Michael Savage <mikejsavage@gmail.com>
2021-04-02 19:21:42 +0300

    GCC warning

--- libs/stb.lua
+++ libs/stb.lua
@@ -8,6 +8,7 @@ gcc_obj_cxxflags( "libs/stb/stb_image.cpp", "-O2 -Wno-type-limits" )
 obj_cxxflags( "libs/stb/stb_image.cpp", "-DSTBI_NO_BMP -DSTBI_NO_GIF -DSTBI_NO_HDR -DSTBI_NO_LINEAR -DSTBI_NO_PIC -DSTBI_NO_PNM -DSTBI_NO_PSD -DSTBI_NO_TGA" )
 
 lib( "stb_image_write", { "libs/stb/stb_image_write.cpp" } )
+gcc_obj_cxxflags( "libs/stb/stb_image_write.cpp", "-Wno-sign-compare" )
 
 lib( "stb_rect_pack", { "libs/stb/stb_rect_pack.cpp" } )
 

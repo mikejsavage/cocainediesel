2c5deaecc7f1bb3e51b49cf6796b9091561e0090
Michael Savage <mikejsavage@gmail.com>
2021-05-02 21:54:53 +0300

    Nerf minigun knockback

--- source/gameshared/gs_weapondefs.cpp
+++ source/gameshared/gs_weapondefs.cpp
@@ -634,7 +634,7 @@ const WeaponDef gs_weaponDefs[] = {
 		// damages
 		/* damage               */ 9,
 		/* self damage          */ 0,
-		/* knockback            */ 45,
+		/* knockback            */ 15,
 		/* splash radius        */ 0,
 		/* splash min damage    */ 0,
 		/* splash min knockback */ 0,

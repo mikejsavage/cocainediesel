01cf9a2f4a1e9fd965a0f3eefce0ea12c6e010ea
Michael Savage <mikejsavage@gmail.com>
2021-08-18 22:35:54 +0300

    Demos aren't saved to base anymore

--- source/client/cl_demo.cpp
+++ source/client/cl_demo.cpp
@@ -327,7 +327,7 @@ static void CL_StartDemo( const char *demoname, bool yolo ) {
 	}
 
 	if( filename ) {
-		tempdemofilelen = FS_FOpenFile( filename, &tempdemofilehandle, FS_READ | SNAP_DEMO_GZ );  // open the demo file
+		tempdemofilelen = FS_FOpenBaseFile( filename, &tempdemofilehandle, FS_READ | SNAP_DEMO_GZ );  // open the demo file
 	}
 
 	if( !tempdemofilehandle ) {
@@ -379,7 +379,7 @@ static void CL_StartDemo( const char *demoname, bool yolo ) {
 * CL_DemoComplete
 */
 const char **CL_DemoComplete( const char *partial ) {
-	return Cmd_CompleteHomeDirFileList( partial, "base/demos", APP_DEMO_EXTENSION_STR );
+	return Cmd_CompleteHomeDirFileList( partial, "demos", APP_DEMO_EXTENSION_STR );
 }
 
 /*

083280e670e85ba4f11e5a15bd1e9cc13f05ae3c
Michael Savage <mikejsavage@gmail.com>
2021-11-13 03:30:43 +0200

    None of this is Id code anymore

--- source/game/g_spawnpoints.cpp
+++ source/game/g_spawnpoints.cpp
@@ -1,23 +1,3 @@
-/*
-Copyright (C) 1997-2001 Id Software, Inc.
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-*/
-
 #include "game/g_local.h"
 #include "qcommon/cmodel.h"
 #include "qcommon/rng.h"
@@ -39,6 +19,7 @@ void DropSpawnToFloor( edict_t * ent ) {
 		G_FreeEdict( ent );
 		return;
 	}
+
 	// move it 1 unit away from the plane
 	ent->s.origin = tr.endpos + tr.plane.normal;
 }

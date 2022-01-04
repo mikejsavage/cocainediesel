40e68513aefd01b1bd08fa88583e6808d94745be
ObaniGemini <robin@pi-et-ro.net>
2022-01-04 12:56:01 +0100

    Better jetpack

--- source/gameshared/pmove_jetpack.cpp
+++ source/gameshared/pmove_jetpack.cpp
@@ -3,51 +3,50 @@
 
 
 static constexpr float pm_defaultspeed = 320.0f;
-
 static constexpr float pm_sidewalkspeed = 320.0f;
 static constexpr float pm_crouchedspeed = 160.0f;
 
-static constexpr float pm_basejumpspeed = 150.0f;
-static constexpr float pm_jetpackspeed = 20.0f;
-static constexpr s16 pm_jetpackmax = 100;
+static constexpr float pm_jetpackspeed = 25.0f;
+static constexpr float pm_maxjetpackupspeed = 600.0f;
 
-static constexpr float pm_boostupspeed = 100.0f;
-static constexpr float pm_boostspeed = 700.0f;
-
+static constexpr float pm_boostspeed = 15.0f;
+static constexpr float pm_boostupspeed = 18.0f;
 
 
+static constexpr s16 ground_refuel = 8;
+static constexpr s16 air_refuel = 1;
 
+static constexpr s16 jetpack_fuel_usage = 4;
+static constexpr s16 boost_fuel_usage = 8;
+
+static constexpr s16 maxfuel = 200;
+static constexpr s16 jetpackmax = maxfuel - ground_refuel;
+
+
+
+
+static void PM_JetpackRefuel( pmove_t * pm, SyncPlayerState * ps ) {
+	if( ps->pmove.special_count > 0 ) {
+		if( pm->groundentity != -1 || pm->waterlevel >= 2 ) {
+			ps->pmove.special_count -= ground_refuel;
+		} else {
+			ps->pmove.special_count -= air_refuel;
+		}
+	}
+}
 
 
 
 static void PM_JetpackJump( pmove_t * pm, pml_t * pml, const gs_state_t * pmove_gs, SyncPlayerState * ps ) {
-	if( ( pm->groundentity != -1 || pm->waterlevel >= 2 ) && ps->pmove.special_count > 0 ) {
-		ps->pmove.special_count -= 3;
-	}
-
-	if( pm->playerState->pmove.pm_type != PM_NORMAL ) {
-		return;
-	}
-
-	if( !( pm->playerState->pmove.features & PMFEAT_JUMP ) ) {
-		return;
-	}
-
-	if( pml->upPush < 10 ) {
-		return;
-	}
-
-	if( pm->groundentity != -1 ) {
-		float jumpSpeed = ( pm->waterlevel >= 2 ? pm_basejumpspeed * 2 : pm_basejumpspeed );
-		pmove_gs->api.PredictedEvent( pm->playerState->POVnum, EV_JUMP, 0 );
-		pml->velocity.z = Max2( 0.0f, pml->velocity.z ) + jumpSpeed;
+	if( pm->playerState->pmove.pm_type == PM_NORMAL &&
+		pm->playerState->pmove.features & PMFEAT_JUMP &&
+		pml->upPush > 10 &&
+		ps->pmove.special_count < jetpackmax )
+	{
 		pm->groundentity = -1;
-	} else if( ps->pmove.special_count < pm_jetpackmax ) {
-		ps->pmove.special_count++;
-		pml->velocity.z += pm_jetpackspeed;
+		ps->pmove.special_count += jetpack_fuel_usage;
+		pml->velocity.z = Min2( pml->velocity.z + pm_jetpackspeed, pm_maxjetpackupspeed );
 	}
-
-	ps->pmove.special_time = 0;
 }
 
 
@@ -59,34 +58,27 @@ static void PM_JetpackSpecial( pmove_t * pm, pml_t * pml, const gs_state_t * pmo
 		pm->playerState->pmove.pm_flags &= ~PMF_SPECIAL_HELD;
 	}
 
-	if( GS_GetWeaponDef( ps->weapon )->zoom_fov != 0 && ( ps->pmove.features & PMFEAT_SCOPE ) != 0 ) {
-		return;
-	}
-
-	if( pm->playerState->pmove.pm_type != PM_NORMAL ) {
-		return;
-	}
-
-	if( ps->pmove.special_count >= pm_jetpackmax ) {
-		return;
-	}
-
-	if( pressed && ( pm->playerState->pmove.features & PMFEAT_SPECIAL ) ) {
+	if( ( GS_GetWeaponDef( ps->weapon )->zoom_fov == 0 || ( ps->pmove.features & PMFEAT_SCOPE ) == 0 ) &&
+		pm->playerState->pmove.pm_type == PM_NORMAL &&
+		ps->pmove.special_count < jetpackmax &&
+		pressed && ( pm->playerState->pmove.features & PMFEAT_SPECIAL ) )
+	{
 		Vec3 dashdir = pml->flatforward;
 		pml->forwardPush = pm_boostspeed;
 
 		dashdir = Normalize( dashdir );
 		dashdir *= pm_boostspeed;
 
-		pml->velocity.x = dashdir.x;
-		pml->velocity.y = dashdir.y;
-		if( pm->groundentity != -1 ) {
-			pml->velocity.z = Max2( 0.0f, pml->velocity.z ) + pm_boostupspeed;
-			pm->groundentity = -1;
-		}
+		pml->velocity.x += dashdir.x;
+		pml->velocity.y += dashdir.y;
+		pml->velocity.z += pm_boostupspeed;
+		pm->groundentity = -1;
 
-		ps->pmove.special_count += 2;
+		ps->pmove.special_count += boost_fuel_usage;
 	}
+
+
+	PM_JetpackRefuel( pm, ps );
 }
 
 

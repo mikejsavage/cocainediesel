76708bcfa3f7d1322579442b9e4b63bd22b4ae92
Michael Savage <mikejsavage@gmail.com>
2022-02-06 00:21:21 +0200

    Add Luau Linux bins

--- libs/luau.lua
+++ libs/luau.lua
@@ -1,1 +1,5 @@
-prebuilt_lib( "luau", { "Luau.AST", "Luau.Compiler", "Luau.VM" } )
+if OS == "windows" then
+	prebuilt_lib( "luau", { "Luau.AST", "Luau.Compiler", "Luau.VM" } )
+else
+	prebuilt_lib( "luau", { "luaucompiler", "luauast", "luauvm" } )
+end

6c5b68b35d2d5b6d830f6bbe50f08148c5b34dd3
Michael Savage <mike@mikejsavage.co.uk>
2022-03-12 17:19:54 +0200

    Move ClientCommandType into gs_synctypes.h

--- source/gameshared/gs_public.h
+++ source/gameshared/gs_public.h
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "gameshared/q_collision.h"
 #include "gameshared/q_math.h"
 #include "gameshared/gs_synctypes.h"
-#include "gameshared/events.h"
 
 //===============================================================
 //		WARSOW player AAboxes sizes

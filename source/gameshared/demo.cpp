61389d27466f2be0876b6f07c214758d8ddce698
Michael Savage <mike@mikejsavage.co.uk>
2022-04-24 01:21:59 +0300

    Fix Linux build

--- source/gameshared/demo.cpp
+++ source/gameshared/demo.cpp
@@ -17,6 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+#include <errno.h>
+
 #include "qcommon/base.h"
 #include "qcommon/version.h"
 #include "qcommon/array.h"

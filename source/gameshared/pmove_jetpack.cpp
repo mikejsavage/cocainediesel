7a16f50ce7bb3f0a535be88f29c4a5c11b0f2ae7
ObaniGemini <robin@pi-et-ro.net>
2022-05-01 14:48:33 +0200

    Fix jetpack getting stuck on the floor

--- source/gameshared/pmove_jetpack.cpp
+++ source/gameshared/pmove_jetpack.cpp
@@ -20,7 +20,7 @@ static constexpr float refuel_air = 0.0f;
 
 static void PM_JetpackJump( pmove_t * pm, pml_t * pml, const gs_state_t * pmove_gs, SyncPlayerState * ps, bool pressed ) {
 	if( pressed ) {
-		if( pm->groundentity != -1 && !( ps->pmove.pm_flags & PMF_ABILITY1_HELD ) ) {
+		if( pm->groundentity != -1 ) {
 			Jump( pm, pml, pmove_gs, ps, pm_jumpspeed, JumpType_Normal, true );
 		}
 

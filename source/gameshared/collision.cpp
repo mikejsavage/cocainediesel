fb2d2b52b3c3e6d474b99f820e9e9ec22c4e1c49
Michael Savage <mike@mikejsavage.co.uk>
2022-06-10 01:32:16 +0300

    add CollisionModelStorage WIP stuff

---
+++ source/gameshared/collision.cpp
@@ -0,0 +1,162 @@
+#include "qcommon/base.h"
+#include "qcommon/array.h"
+#include "qcommon/hash.h"
+#include "qcommon/string.h"
+#include "gameshared/cdmap.h"
+#include "gameshared/collision.h"
+#include "gameshared/q_collision.h"
+
+#include "cgltf/cgltf.h"
+
+static void DeleteGLTFCollisionData( GLTFCollisionData data ) {
+	FREE( sys_allocator, data.vertices.ptr );
+	FREE( sys_allocator, data.planes.ptr );
+	FREE( sys_allocator, data.brushes.ptr );
+}
+
+void InitCollisionModelStorage( CollisionModelStorage * storage ) {
+	storage->gltfs_hashtable.clear();
+	storage->maps_hashtable.clear();
+	storage->map_models_hashtable.clear();
+}
+
+void ShutdownCollisionModelStorage( CollisionModelStorage * storage ) {
+	for( size_t i = 0; i < storage->gltfs_hashtable.size(); i++ ) {
+		DeleteGLTFCollisionData( storage->gltfs[ i ] );
+	}
+}
+
+struct EditorMaterial {
+	StringHash name;
+	bool visible;
+	u8 solidity;
+};
+
+static constexpr u8 CONTENTS_LADDER = 1234;
+constexpr const EditorMaterial editor_materials[] = {
+	{ "discard", false, CONTENTS_SOLID },
+	{ "ladder", false, CONTENTS_LADDER | CONTENTS_PLAYERCLIP },
+	{ "clip", false, CONTENTS_SOLID },
+	{ "playerclip", false, CONTENTS_PLAYERCLIP },
+	{ "weaponclip", false, CONTENTS_WEAPONCLIP },
+	{ "trigger", false, CONTENTS_SOLID },
+	{ "wallbangable", true, CONTENTS_WALLBANGABLE },
+	{ "door", true, CONTENTS_WEAPONCLIP },
+};
+
+static const EditorMaterial * FindEditorMaterial( StringHash name ) {
+	for( const EditorMaterial & material : editor_materials ) {
+		if( material.name == name ) {
+			return &material;
+		}
+	}
+
+	return NULL;
+}
+
+static bool IsConvex( GLTFCollisionData data, GLTFCollisionBrush brush ) {
+	for( u32 i = 0; i < brush.num_planes; i++ ) {
+		Plane plane = data.planes[ brush.first_plane + i ];
+		for( u32 j = 0; j < brush.num_vertices; j++ ) {
+			Vec3 v = data.vertices[ brush.first_vertex + j ];
+			// TODO: probably needs epsilon
+			if( Dot( plane.normal, v ) - plane.distance > 0 ) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool LoadGLTFCollisionData( CollisionModelStorage * storage, const cgltf_data * gltf, const char * path, u64 hash ) {
+	NonRAIIDynamicArray< Vec3 > vertices( sys_allocator );
+	NonRAIIDynamicArray< Plane > planes( sys_allocator );
+	NonRAIIDynamicArray< GLTFCollisionBrush > brushes( sys_allocator );
+
+	for( size_t i = 0; i < gltf->nodes_count; i++ ) {
+		const cgltf_node * node = &gltf->nodes[ i ];
+		if( node->mesh == NULL )
+			continue;
+
+		const cgltf_primitive * prim = &node->mesh->primitives[ 0 ];
+		if( prim->material == NULL )
+			continue;
+
+		const EditorMaterial * material = FindEditorMaterial( StringHash( prim->material->name ) );
+		if( material == NULL )
+			continue;
+
+		Mat4 transform;
+		cgltf_node_transform_world( node, transform.ptr() );
+
+		// TODO: make post-transform hull (deduped verts)
+		// TODO: make planes from post-transform triangles
+		// TODO: sort by planes and merge similar
+	}
+
+	if( brushes.size() == 0 ) {
+		return true;
+	}
+
+	GLTFCollisionData data;
+	data.vertices = vertices.span();
+	data.planes = planes.span();
+	data.brushes = brushes.span();
+
+	for( GLTFCollisionBrush brush : data.brushes ) {
+		if( !IsConvex( data, brush ) ) {
+			DeleteGLTFCollisionData( data );
+			return false;
+		}
+	}
+
+	u64 idx = storage->gltfs_hashtable.size();
+	if( !storage->gltfs_hashtable.get( hash, &idx ) ) {
+		storage->gltfs_hashtable.add( hash, storage->gltfs_hashtable.size() );
+	}
+	else {
+		DeleteGLTFCollisionData( storage->gltfs[ idx ] );
+	}
+
+	storage->gltfs[ idx ] = data;
+
+	return true;
+}
+
+static void FillMapModelsHashtable( CollisionModelStorage * storage ) {
+	TracyZoneScoped;
+
+	storage->map_models_hashtable.clear();
+
+	for( u32 i = 0; i < storage->maps_hashtable.size(); i++ ) {
+		const MapSharedCollisionData * map = &storage->maps[ i ];
+		for( size_t j = 0; j < map->data.models.n; j++ ) {
+			String< 16 > suffix( "*{}", j );
+			u64 hash = Hash64( suffix.c_str(), suffix.length(), map->base_hash );
+
+			assert( storage->map_models_hashtable.size() < ARRAY_COUNT( storage->map_models ) ); // TODO: check in release builds too
+			storage->map_models[ storage->map_models_hashtable.size() ] = {
+				StringHash( map->base_hash ),
+				checked_cast< u32 >( j )
+			};
+			storage->map_models_hashtable.add( hash, storage->map_models_hashtable.size() );
+		}
+	}
+}
+
+void LoadMapCollisionData( CollisionModelStorage * storage, const MapData * data, u64 hash ) {
+	TracyZoneScoped;
+
+	u64 idx = storage->maps_hashtable.size();
+	if( !storage->maps_hashtable.get( hash, &idx ) ) {
+		storage->maps_hashtable.add( hash, storage->maps_hashtable.size() );
+	}
+
+	MapSharedCollisionData map;
+	map.base_hash = hash;
+	map.data = *data;
+	storage->maps[ idx ] = map;
+
+	FillMapModelsHashtable( storage );
+}
---
+++ source/gameshared/collision.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "qcommon/types.h"
+#include "qcommon/hash.h"
+#include "qcommon/hashtable.h"
+
+struct GLTFCollisionBrush {
+	u32 first_plane;
+	u32 num_planes;
+	u32 first_vertex;
+	u32 num_vertices;
+};
+
+struct GLTFCollisionData {
+	Span< Vec3 > vertices;
+	Span< Plane > planes;
+	Span< GLTFCollisionBrush > brushes;
+};
+
+struct MapSubModelCollisionData {
+	StringHash base_hash;
+	u32 sub_model;
+};
+
+struct MapSharedCollisionData {
+	// TODO: put a copy of the asset in here?
+	u64 base_hash;
+	MapData data;
+};
+
+struct CollisionModelStorage {
+	static constexpr size_t MAX_GLTF_COLLISION_MODELS = 1024;
+
+	GLTFCollisionData gltfs[ MAX_GLTF_COLLISION_MODELS ];
+	Hashtable< MAX_GLTF_COLLISION_MODELS * 2 > gltfs_hashtable;
+
+	static constexpr size_t MAX_MAPS = 128;
+	static constexpr size_t MAX_MAP_MODELS = 1024;
+
+	MapSharedCollisionData maps[ MAX_MAPS ];
+	Hashtable< MAX_MAPS * 2 > maps_hashtable;
+
+	MapSubModelCollisionData map_models[ MAX_MAP_MODELS ];
+	Hashtable< MAX_MAP_MODELS * 2 > map_models_hashtable;
+};
+
+void InitCollisionModelStorage( CollisionModelStorage * storage );
+void ShutdownCollisionModelStorage( CollisionModelStorage * storage );
+
+struct cgltf_data;
+bool LoadGLTFCollisionData( CollisionModelStorage * storage, const cgltf_data * gltf, const char * path, u64 hash );
+
+struct Mapdata;
+void LoadMapCollisionData( CollisionModelStorage * storage, const MapData * map, u64 hash );

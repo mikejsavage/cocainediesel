11d137f03036f01025bfcb339517d371f9c3558f
Michael Savage <mike@mikejsavage.co.uk>
2022-07-15 00:24:30 +0300

    Prebuild discord-rpc, works around libc++ link errors with zig

--- libs/discord/make.lua
+++ libs/discord/make.lua
@@ -1,18 +1,1 @@
-local windows_srcs = {
-	"libs/discord/connection_win.cpp",
-	"libs/discord/discord_register_win.cpp",
-}
-
-local linux_srcs = {
-	"libs/discord/connection_unix.cpp",
-	"libs/discord/discord_register_linux.cpp",
-}
-
-lib( "discord", {
-	OS == "windows" and windows_srcs or linux_srcs,
-	"libs/discord/discord_rpc.cpp",
-	"libs/discord/rpc_connection.cpp",
-	"libs/discord/serialization.cpp",
-} )
-
-gcc_obj_cxxflags( "libs/discord/.*", "-w" )
+prebuilt_lib( "discord" )

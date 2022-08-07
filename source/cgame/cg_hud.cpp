6043779e51ae1161efac7f47bab002ece4db8847
Michael Savage <mike@mikejsavage.co.uk>
2022-08-08 00:39:00 +0300

    Don't leak yoga memory when the luau HUD doesn't compile

--- source/cgame/cg_hud.cpp
+++ source/cgame/cg_hud.cpp
@@ -1644,9 +1644,10 @@ void CG_InitHUD() {
 void CG_ShutdownHUD() {
 	if( hud_L != NULL ) {
 		lua_close( hud_L );
-		YGConfigFree( yoga_config );
-		FREE( sys_allocator, yoga_arena_memory );
 	}
+
+	YGConfigFree( yoga_config );
+	FREE( sys_allocator, yoga_arena_memory );
 
 	RemoveCommand( "toggleuiinspector" );
 }

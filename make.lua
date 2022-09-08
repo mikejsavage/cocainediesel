e82eabe282ee0ae24b5b552d2b6304a7e31e441e
Michael Savage <mike@mikejsavage.co.uk>
2022-09-08 21:09:40 +0300

    Stop using fast math
    We almost universally choose safety over program-wide micro
    optimisations, let's be consistent here too.

--- make.lua
+++ make.lua
@@ -6,9 +6,9 @@ global_cxxflags( "-I source -I libs" )
 msvc_global_cxxflags( "/std:c++17 /W4 /wd4100 /wd4146 /wd4189 /wd4201 /wd4307 /wd4324 /wd4351 /wd4127 /wd4505 /wd4530 /wd4702 /wd4706 /D_CRT_SECURE_NO_WARNINGS" )
 msvc_global_cxxflags( "/wd4244 /wd4267" ) -- silence conversion warnings because there are tons of them
 msvc_global_cxxflags( "/wd4611" ) -- setjmp warning
-msvc_global_cxxflags( "/fp:fast /GR- /EHs-c-" )
+msvc_global_cxxflags( "/GR- /EHs-c-" )
 
-gcc_global_cxxflags( "-std=c++17 -msse3 -ffast-math -fno-exceptions -fno-rtti -fno-strict-aliasing -fno-strict-overflow -fvisibility=hidden" )
+gcc_global_cxxflags( "-std=c++17 -msse3 -fno-exceptions -fno-rtti -fno-strict-aliasing -fno-strict-overflow -fvisibility=hidden" )
 gcc_global_cxxflags( "-Wall -Wextra -Wcast-align -Wvla -Wformat-security" ) -- -Wconversion
 gcc_global_cxxflags( "-Wno-unused-parameter -Wno-missing-field-initializers -Wno-implicit-fallthrough" )
 gcc_global_cxxflags( "-Werror=vla -Werror=format-security -Werror=unused-value" )

9b7d2d9345e46d4d6417d324351a04214b3ec00b
Michael Savage <mike@mikejsavage.co.uk>
2023-01-07 23:09:29 +0200

    Windows Intel driver seems to be ignoring the vertex attrib format
    Tested on my mom's PC, the numbers in Renderdoc look fine but in game
    you get huge joint indices, which are also valid if you `>> 16` them, so
    I guess the driver just interprets them as u32
    
    Skinned models still don't render quite right but it's not gamebreaking
    so this is good enough for now

--- base/glsl/include/skinning.glsl
+++ base/glsl/include/skinning.glsl
@@ -9,11 +9,13 @@ layout( std140 ) uniform u_Pose {
 };
 
 void Skin( inout vec4 position, inout vec3 normal ) {
+	// the & 127 is to work around a driver bug, Wintel ignores our vertex
+	// attrib format and treats them as u32
 	mat4 skin =
-		a_JointWeights.x * u_SkinningMatrices[ a_JointIndices.x ] +
-		a_JointWeights.y * u_SkinningMatrices[ a_JointIndices.y ] +
-		a_JointWeights.z * u_SkinningMatrices[ a_JointIndices.z ] +
-		a_JointWeights.w * u_SkinningMatrices[ a_JointIndices.w ];
+		a_JointWeights.x * u_SkinningMatrices[ a_JointIndices.x & 127 ] +
+		a_JointWeights.y * u_SkinningMatrices[ a_JointIndices.y & 127 ] +
+		a_JointWeights.z * u_SkinningMatrices[ a_JointIndices.z & 127 ] +
+		a_JointWeights.w * u_SkinningMatrices[ a_JointIndices.w & 127 ];
 
 	position = skin * position;
 	normal = normalize( mat3( skin ) * normal );

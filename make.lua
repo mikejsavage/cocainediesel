7587bf773d8fe08f25fc77e803421182e8580697
Michael Savage <mike@mikejsavage.co.uk>
2023-01-19 12:04:08 +0200

    -msse4.2

--- make.lua
+++ make.lua
@@ -8,7 +8,7 @@ msvc_global_cxxflags( "/wd4244 /wd4267" ) -- silence conversion warnings because
 msvc_global_cxxflags( "/wd4611" ) -- setjmp warning
 msvc_global_cxxflags( "/GR- /EHs-c-" )
 
-gcc_global_cxxflags( "-std=c++17 -msse3 -fno-exceptions -fno-rtti -fno-strict-aliasing -fno-strict-overflow -fno-math-errno -fvisibility=hidden" )
+gcc_global_cxxflags( "-std=c++17 -msse4.2 -fno-exceptions -fno-rtti -fno-strict-aliasing -fno-strict-overflow -fno-math-errno -fvisibility=hidden" )
 gcc_global_cxxflags( "-Wall -Wextra -Wcast-align -Wvla -Wformat-security" ) -- -Wconversion
 gcc_global_cxxflags( "-Wno-unused-parameter -Wno-missing-field-initializers -Wno-implicit-fallthrough" )
 gcc_global_cxxflags( "-Werror=vla -Werror=format-security -Werror=unused-value" )

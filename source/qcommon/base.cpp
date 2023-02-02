3b2162c18e39188a878cb0b0c445abe92a77770a
Michael Savage <mike@mikejsavage.co.uk>
2023-02-02 15:39:58 +0200

    Accurate location info in FatalErrno

--- source/qcommon/base.cpp
+++ source/qcommon/base.cpp
@@ -21,8 +21,8 @@ void FatalImpl( const char * file, int line, const char * format, ... ) {
 	abort();
 }
 
-void FatalErrno( const char * msg ) {
-	Fatal( "%s: %s (%d)", msg, strerror( errno ), errno );
+void FatalErrnoImpl( const char * msg, const char * file, int line ) {
+    FatalImpl( file, line, "%s: %s (%d)", msg, strerror( errno ), errno );
 }
 
 void AssertFail( const char * str, const char * file, int line ) {

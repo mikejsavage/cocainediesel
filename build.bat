1b4e1eabb88c4e4e83123a08fff08e4da4478366
Michael Savage <mike@mikejsavage.co.uk>
2023-02-05 16:58:26 +0200

    Switch Appveyor to VS2022, use the 64bit compiler

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 @set vcvarsall="C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Auxiliary\\Build\\vcvarsall.bat"
 @if not defined INCLUDE (
-	if exist %vcvarsall% call %vcvarsall% x86_amd64
+	if exist %vcvarsall% call %vcvarsall% amd64
 )
 
 @set vcvarsall="C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat"
 @if not defined INCLUDE (
-	if exist %vcvarsall% call %vcvarsall% x86_amd64
+	if exist %vcvarsall% call %vcvarsall% amd64
 )
 
 @set vcvarsall="C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat"
 @if not defined INCLUDE (
-	if exist %vcvarsall% call %vcvarsall% x86_amd64
+	if exist %vcvarsall% call %vcvarsall% amd64
 )
 
 ggbuild\lua.exe make.lua > build.ninja

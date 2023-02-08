7cb795d8ba3fa03270688cc7cbbd9b2191441b3a
Michael Savage <mike@mikejsavage.co.uk>
2023-02-08 14:16:28 +0200

    Little cleanup
    constexpr also applies const. In `const T a[]`, the const applies to the
    data and the pointer is automatically const.

--- source/cgame/cg_hud.cpp
+++ source/cgame/cg_hud.cpp
@@ -399,7 +399,7 @@ static const Material * DamageTypeToIcon( DamageType type ) {
 //=============================================================================
 
 static void GlitchText( Span< char > msg ) {
-	constexpr const char glitches[] = { '#', '@', '~', '$' };
+	constexpr char glitches[] = { '#', '@', '~', '$' };
 
 	RNG rng = NewRNG( cls.monotonicTime.flicks / ( GGTIME_FLICKS_PER_SECOND / 14 ), 0 );
 
@@ -698,7 +698,7 @@ static int LuauDraw2DBoxUV( lua_State * L ) {
 }
 
 static Alignment CheckAlignment( lua_State * L, int idx ) {
-	constexpr const Alignment alignments[] = {
+	constexpr Alignment alignments[] = {
 		Alignment_LeftTop,
 		Alignment_CenterTop,
 		Alignment_RightTop,
@@ -1184,7 +1184,7 @@ static void CheckYogaNumberAllEdges( lua_State * L, int idx, const char * key, Y
 }
 
 static YGFlexDirection CheckYogaFlexDirection( lua_State * L, int idx ) {
-	constexpr const YGFlexDirection flex_directions[] = {
+	constexpr YGFlexDirection flex_directions[] = {
 		YGFlexDirectionRow,
 		YGFlexDirectionRowReverse,
 		YGFlexDirectionColumn,
@@ -1202,7 +1202,7 @@ static YGFlexDirection CheckYogaFlexDirection( lua_State * L, int idx ) {
 }
 
 static YGAlign CheckYogaAlign( lua_State * L, int idx ) {
-	constexpr const YGAlign aligns[] = {
+	constexpr YGAlign aligns[] = {
 		YGAlignStretch,
 		YGAlignFlexStart,
 		YGAlignCenter,
@@ -1226,7 +1226,7 @@ static YGAlign CheckYogaAlign( lua_State * L, int idx ) {
 }
 
 static YGJustify CheckYogaJustify( lua_State * L, int idx ) {
-	constexpr const YGJustify justifies[] = {
+	constexpr YGJustify justifies[] = {
 		YGJustifyFlexStart,
 		YGJustifyCenter,
 		YGJustifyFlexEnd,
@@ -1549,7 +1549,7 @@ void CG_InitHUD() {
 		Fatal( "luaL_newstate" );
 	}
 
-	constexpr const luaL_Reg cdlib[] = {
+	constexpr luaL_Reg cdlib[] = {
 		{ "print", LuauPrint },
 		{ "asset", LuauAsset },
 		{ "box", LuauDraw2DBox },

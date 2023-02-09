48582042e501ba9db0920c47b7fb6eb2f89c063d
Michael Savage <mike@mikejsavage.co.uk>
2023-02-09 21:52:39 +0200

    Better fix + explanation for d0ab92f7cd00d "WTF"
    `constexpr char asdf[] = "hello";` seems to just ignore the constexpr
    requirement and reconstructs the string at runtime. In debug builds it
    would then return to-be-corrupted stack data, in release builds it got
    nuked for being UB.
    
    `constexpr const char *` embeds the literal as a readonly constant as
    you would expect.

--- source/client/keys.cpp
+++ source/client/keys.cpp
@@ -129,7 +129,7 @@ int Key_StringToKeynum( const char *str ) {
 }
 
 Span< const char > Key_KeynumToString( int keynum ) {
-	static constexpr char uppercase_ascii[] = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`ABCDEFGHIJKLMNOPQRSTUVWXYZ{|}~";
+	constexpr const char * uppercase_ascii = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`ABCDEFGHIJKLMNOPQRSTUVWXYZ{|}~";
 
 	if( keynum >= '!' && keynum <= '~' ) {
 		return Span< const char >( uppercase_ascii + keynum - '!', 1 );

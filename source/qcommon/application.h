e98d0fa56876cdfb3a2220ca163e44498485df80
Michael Savage <mike@mikejsavage.co.uk>
2023-02-23 00:03:42 +0200

    It's 2023 + little cleanup

--- source/qcommon/application.h
+++ source/qcommon/application.h
@@ -2,12 +2,6 @@
 
 #define APPLICATION                     "Cocaine Diesel"
 #define APPLICATION_NOSPACES            "CocaineDiesel"
-#define DEFAULT_BASEGAME                "base"
-
-#define APP_URL                         "https://cocainediesel.fun/"
-
-#define APP_COPYRIGHT_OWNER             "Aha Cheers"
 
 #define APP_DEMO_EXTENSION_STR          ".cddemo"
-
 #define APP_URI_SCHEME                  "diesel://"

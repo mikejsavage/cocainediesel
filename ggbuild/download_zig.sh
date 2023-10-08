db87186e019ef7fdc77429d88c0ef71def1c442d
Michael Savage <mike@mikejsavage.co.uk>
2023-10-08 19:18:01 +0300

    Fix Linux CI

--- ggbuild/download_zig.sh
+++ ggbuild/download_zig.sh
@@ -1,12 +1,13 @@
 #! /usr/bin/env bash
 
-set -eou pipefail
+set -eoux pipefail
 
 cd "$(dirname "$0")"
+ls
 
 version="$(cat zig_version.txt)"
 
-if [ -d "zig-$version" ]; then
+if [ -e "zig-$version/zig" ]; then
 	exit
 fi
 
@@ -14,4 +15,6 @@ wget --continue "https://ziglang.org/download/$version/zig-linux-x86_64-$version
 echo "2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047 zig-linux-x86_64-$version.tar.xz" | sha256sum -c
 tar xf "zig-linux-x86_64-$version.tar.xz"
 rm "zig-linux-x86_64-$version.tar.xz"
-mv "zig-linux-x86_64-$version/" "zig-$version"
+
+# ninja makes the zig dir for some reason, --no-target-directory overwrites an existing empty dir
+mv --no-target-directory "zig-linux-x86_64-$version" "zig-$version"

f3ddc4407834a473239cda7ba5192ad7361c0e22
Michael Savage <mike@mikejsavage.co.uk>
2023-12-14 14:28:49 +0000

    Fix download_zig.sh as root

--- ggbuild/download_zig.sh
+++ ggbuild/download_zig.sh
@@ -13,7 +13,7 @@ fi
 
 wget --continue "https://ziglang.org/download/$version/zig-linux-x86_64-$version.tar.xz"
 echo "2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047 zig-linux-x86_64-$version.tar.xz" | sha256sum -c
-tar xf "zig-linux-x86_64-$version.tar.xz"
+tar --touch --no-same-owner -xf "zig-linux-x86_64-$version.tar.xz"
 rm "zig-linux-x86_64-$version.tar.xz"
 
 # ninja makes the zig dir for some reason, --no-target-directory overwrites an existing empty dir

6130de98ff2f83ece65d09d0d4e374bd3fc46f96
Michael Savage <mike@mikejsavage.co.uk>
2024-01-15 00:43:07 +0200

    Full zig Linux builds
    Also updates several libraries but splitting this commit would be too painful
    
    The game doesn't quite build in a FROM scratch container but we should
    be isolated enough from the host OS to have robust builds now.

--- ggbuild/download_zig.sh
+++ ggbuild/download_zig.sh
@@ -1,9 +1,8 @@
 #! /usr/bin/env bash
 
-set -eoux pipefail
+set -eou pipefail
 
 cd "$(dirname "$0")"
-ls
 
 version="$(cat zig_version.txt)"
 
@@ -11,7 +10,7 @@ if [ -e "zig-$version/zig" ]; then
 	exit
 fi
 
-wget --continue "https://ziglang.org/download/$version/zig-linux-x86_64-$version.tar.xz"
+wget --continue --no-verbose "https://ziglang.org/download/$version/zig-linux-x86_64-$version.tar.xz"
 echo "2d00e789fec4f71790a6e7bf83ff91d564943c5ee843c5fd966efc474b423047 zig-linux-x86_64-$version.tar.xz" | sha256sum -c
 tar --touch --no-same-owner -xf "zig-linux-x86_64-$version.tar.xz"
 rm "zig-linux-x86_64-$version.tar.xz"

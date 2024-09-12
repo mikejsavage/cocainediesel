5d154655e5c071f4e0a33b9be88e4ad0d7883910
Michael Savage <mike@mikejsavage.co.uk>
2024-09-12 11:06:33 +0000

    Dedupe platform server code

--- source/server/cl_stubs.cpp
+++ source/server/cl_stubs.cpp
@@ -1,4 +1,5 @@
-#include "qcommon/types.h"
+#include "qcommon/base.h"
+#include "qcommon/qcommon.h"
 
 void CL_Init() { }
 void CL_Shutdown() { }
@@ -10,3 +11,34 @@ void Con_Print( Span< const char > str ) { }
 
 void Key_Init() { }
 void Key_Shutdown() { }
+
+void OSServerInit();
+bool OSServerShouldQuit();
+
+int main( int argc, char ** argv ) {
+	OSServerInit();
+
+	Qcommon_Init( argc, argv );
+
+	s64 oldtime = Sys_Milliseconds();
+	while( true ) {
+		TracyCFrameMark;
+
+		s64 dt = 0;
+		{
+			TracyZoneScopedN( "Interframe" );
+			while( dt == 0 ) {
+				dt = Sys_Milliseconds() - oldtime;
+			}
+			oldtime += dt;
+		}
+
+		if( !Qcommon_Frame( dt ) || OSServerShouldQuit() ) {
+			break;
+		}
+	}
+
+	Qcommon_Shutdown();
+
+	return 0;
+}
--- source/server/platform/windows_server.cpp
+++ source/server/platform/windows_server.cpp
@@ -12,30 +12,11 @@ void ShowErrorMessage( const char * msg, const char * file, int line ) {
 	printf( "%s (%s:%d)\n", msg, file, line );
 }
 
-int main( int argc, char ** argv ) {
-	Qcommon_Init( argc, argv );
+void OSServerInit() {
+}
 
-	s64 oldtime = Sys_Milliseconds();
-	while( true ) {
-		TracyCFrameMark;
-
-		s64 dt = 0;
-		{
-			TracyZoneScopedN( "Interframe" );
-			while( dt == 0 ) {
-				dt = Sys_Milliseconds() - oldtime;
-			}
-			oldtime += dt;
-		}
-
-		if( !Qcommon_Frame( dt ) ) {
-			break;
-		}
-	}
-
-	Qcommon_Shutdown();
-
-	return 0;
+bool OSServerShouldQuit() {
+	return false;
 }
 
 #endif // #if PLATFORM_WINDOWS

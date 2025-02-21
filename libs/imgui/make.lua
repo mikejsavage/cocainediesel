1671af5ce1ee062285f17eb6616a0600c2f37a8e
Michael Savage <mike@mikejsavage.co.uk>
2025-02-21 21:36:53 +0200

    Linux build fixes

--- libs/imgui/make.lua
+++ libs/imgui/make.lua
@@ -1,5 +1,6 @@
 lib( "imgui", { "libs/imgui/**.cpp" } )
 
+obj_cxxflags( "libs/imgui/.*%.cpp", "-I libs/imgui" )
 obj_cxxflags( "libs/imgui/imgui_freetype.cpp", "-I libs/imgui/misc/freetype -I libs/freetype" )
 obj_cxxflags( "libs/imgui/imgui_impl_glfw.cpp", "-I libs/glfw3 -DGLFW_INCLUDE_NONE" )
 obj_cxxflags( "libs/imgui/imgui_impl_sdl3.cpp", "-I libs/sdl" )

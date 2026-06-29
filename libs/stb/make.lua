00842664adcdee8bd2cb6345f3701d20058d37c0
Michael Savage <mike@mikejsavage.co.uk>
2026-06-29 12:58:24 +0000

    O2 all stb stuff on Windows too

--- libs/stb/make.lua
+++ libs/stb/make.lua
@@ -1,6 +1,6 @@
 lib( "stb_image", { "libs/stb/stb_image.cpp" } )
 obj_cxxflags( "libs/stb/stb_image.cpp", "-DSTBI_NO_BMP -DSTBI_NO_GIF -DSTBI_NO_HDR -DSTBI_NO_LINEAR -DSTBI_NO_PIC -DSTBI_NO_PNM -DSTBI_NO_PSD -DSTBI_NO_TGA" )
-msvc_obj_cxxflags( "libs/stb/stb_image.cpp", "/O2 /wd4244 /wd4456" )
+msvc_obj_cxxflags( "libs/stb/stb_image.cpp", "/wd4244 /wd4456" )
 
 -- we only use stb_image_resize in bc4 so go hard on the CPU features
 lib( "stb_image_resize", { "libs/stb/stb_image_resize2.cpp" } )
@@ -16,6 +16,7 @@ lib( "stb_rect_pack", { "libs/stb/stb_rect_pack.cpp" } )
 lib( "stb_truetype", { "libs/stb/stb_truetype.cpp" } )
 
 lib( "stb_vorbis", { "libs/stb/stb_vorbis.cpp" } )
-msvc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "/O2 /wd4244 /wd4245 /wd4456 /wd4457 /wd4701" )
+msvc_obj_cxxflags( "libs/stb/stb_vorbis.cpp", "/wd4244 /wd4245 /wd4456 /wd4457 /wd4701" )
 
+msvc_obj_cxxflags( "libs/stb/.*%.cpp", "/O2" )
 gcc_obj_cxxflags( "libs/stb/.*%.cpp", "-O2 -w" )
